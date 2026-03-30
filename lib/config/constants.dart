44bed5cd3b827b382d1fd2597b6c3e67bdfd65ef
zzbzc <zzbzc@3220964425@qq.com>
2026-03-30 07:50:23 +0000

    调整端口格式

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -6,11 +6,11 @@ class AppConfig {
 
   static const String apiPrefix = '/api/v1';
 
-  // API 端点（注意：FastAPI 需要 trailing slash）
-  static String get registerUrl => '$baseUrl$apiPrefix/auth/register/';
-  static String get loginUrl => '$baseUrl$apiPrefix/auth/login/';
-  static String get refreshTokenUrl => '$baseUrl$apiPrefix/auth/refresh/';
-  static String get userProfileUrl => '$baseUrl$apiPrefix/auth/me/';
+  // API 端点
+  static String get registerUrl => '$baseUrl$apiPrefix/auth/register';
+  static String get loginUrl => '$baseUrl$apiPrefix/auth/login';
+  static String get refreshTokenUrl => '$baseUrl$apiPrefix/auth/refresh';
+  static String get userProfileUrl => '$baseUrl$apiPrefix/auth/me';
 
   static String get goalsUrl => '$baseUrl$apiPrefix/goals/';
   static String get checkinsUrl => '$baseUrl$apiPrefix/checkins/';
